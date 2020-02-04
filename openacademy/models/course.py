1535fef6465fb8272207c765c91ddfdcfeacd170
vaidy123 <vaidy@thapovan-inc.com>
2020-02-04 20:03:20 +0000

    Many to Many fields sample

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,4 +1,10 @@
 from odoo import models, fields
+
+class OpenAcademyTags(models.Model):
+    _name = 'openacademy.tags'
+    _description = 'Open Academy Tags'
+    name = fieds.Char(string='Name')
+    
 class OpenAcademyCourse(models.Model):
     _name = 'openacademy.course'
     _description = 'OpenAcademy Course'
@@ -9,3 +15,7 @@ class OpenAcademyCourse(models.Model):
     banner = fields.Binary(string='Banner')
     price = fields.Float(string='Price', digits=(5,4))
     expire_date = fields.Date(string='Expire After', required=True)
+    
+    responsible_id = fields.Many2one(comodel_name='res.users',required=True, string='Responsible Id', ondelete='restrict', copy=False)
+    
+    tag_ids = fields.ManytoMany(comodel_name='openacademy.tags', relation='rel_course_tags', column1='course_id', column2='tag_id', string='Tags')
