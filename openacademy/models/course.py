1384fb8c68c9c08e1d465a2ee28a92559b48e07a
vaidy123 <vaidy@thapovan-inc.com>
2020-02-04 19:22:24 +0000

    [ADD] new fileds to module

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,4 +1,10 @@
-from odoo import models
+from odoo import models, fields
 class OpenAcademyCourse(models.Model):
     _name = 'openacademy.course'
     _description = 'OpenAcademy Course'
+    
+    
+    name = fields.Char(string='Course Title', required=True, index=True, help='Enter Your course title.')
+    description = fields.Html(string='Description')
+    banner = fields.Binary(string='Banner')
+    price = fields.Float(string='Price', digits=(5,4))
