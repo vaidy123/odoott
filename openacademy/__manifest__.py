3017f7c79586ef1549be34a142d33e08e01dfb38
vaidy123 <vaidy@thapovan-inc.com>
2020-02-05 19:16:14 +0000

    inheritance error fixes

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -24,8 +24,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/openacademy_course_views.xml'
-        'views/res_partner_views.xml'
+        'views/openacademy_course_views.xml',
+        'views/res_partner_views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
